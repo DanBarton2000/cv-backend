6a446d5d518baf1dfdfb4d74acce0e37ae32dd4d
Dan Barton <danbarton2000@gmail.com>
2025-02-02 21:40:47 +0000

    Made the lambda function way more efficient

--- lambda_handler.py
+++ lambda_handler.py
@@ -1,38 +1,28 @@
 import json
 import boto3
 
-def lambda_handler(event, context):
-    dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('PersonalWebsiteCounter')
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('PersonalWebsiteCounter')
 
-    response = table.get_item(
-        Key={
-            'Count': 0
-        }
+def lambda_handler(event, context):
+    response = table.update_item(
+        Key={'Count': 0},
+        UpdateExpression='ADD #val :incr',
+        ExpressionAttributeNames={'#val': 'Value'},
+        ExpressionAttributeValues={':incr': 1},
+        ReturnValues='UPDATED_NEW'  # Returns the new counter value
     )
-    
-    value = response['Item']['Value']
-    value += 1
-    
-    table.update_item(
-        Key={
-            'Count': 0
-        },
-        UpdateExpression='SET #val = :val1',
-        ExpressionAttributeNames={
-            "#val": "Value"
-        },
-        ExpressionAttributeValues={
-            ':val1': value
-        }
-    )
+
+    updated_value = response['Attributes']['Value']
 
     return {
         'statusCode': 200,
         'headers': {
-            "Access-Control-Allow-Headers" : "Content-Type",
+            "Access-Control-Allow-Headers": "Content-Type",
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Methods": "GET"
         },
-        'body': value
+        'body': {
+            "count": updated_value
+        }
     }
